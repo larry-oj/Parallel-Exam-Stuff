b8b8ae26f2c298e0017c4329bfa328d412bfa659
Larry <66905431+larry-oj@users.noreply.github.com>
2023-06-16 09:50:24 +0300

    t1 task1 v2

--- ticket1/src/Main.java
+++ ticket1/src/Main.java
@@ -1,12 +1,3 @@
-// Напишіть код, в якому створюються та запускаються на виконання потоки S і W. Потік S виконує переключення з затримкою 1000 мілісекунд зі стану true у стан false і навпаки. Потік W очікує стану true потоку S, виводить на консоль зворотний відлік від 30 з затримкою 100 мілісекунд та призупиняє свою дію, як тільки потік S переключено у стан false. Умовою завершення роботи потоків є досягнення відліку нульової відмітки.
-
-// Напишіть код, в якому створюються та запускаються на виконання потоки S і W.
-// Потік S виконує переключення з затримкою 1000 мілісекунд зі стану true у стан
-// false і навпаки. Потік W очікує стану true потоку S, виводить на консоль
-// зворотний відлік від 30 з затримкою 100 мілісекунд та призупиняє свою дію, як
-// тільки потік S переключено у стан false. Умовою завершення роботи потоків є
-// досягнення відліку нульової відмітки.
-
 public class Main {
     public static void main(String[] args) {
         SThread sThread = new SThread();
@@ -19,14 +10,19 @@ public class Main {
 
 class SThread extends Thread {
     private boolean state = true;
+    private final Object lock = new Object();
 
     @Override
     public void run() {
         while (!isInterrupted()) {
             try {
                 Thread.sleep(1000);
-                state = !state;
-                System.out.println("S state: " + state);
+                synchronized (lock) {
+                    state = !state;
+                    System.out.println("S state: " + state);
+                    lock.notify(); // Notify waiting thread
+                    lock.wait(); // Wait for notification from the other thread
+                }
             } catch (InterruptedException e) {
                 // Handle interruption
                 return;
@@ -36,6 +32,18 @@ class SThread extends Thread {
 
     public boolean getThreadState() {
         return state;
+    }
+
+    public void waitForNotification() throws InterruptedException {
+        synchronized (lock) {
+            lock.wait();
+        }
+    }
+
+    public void notifyThread() {
+        synchronized (lock) {
+            lock.notify();
+        }
     }
 }
 
@@ -53,10 +61,11 @@ class WThread extends Thread {
             for (int i = 30; i >= 0; i--) {
                 // Wait until the state of SThread is true
                 while (!sThread.getThreadState()) {
-                    Thread.sleep(100);
+                    sThread.waitForNotification();
                 }
                 System.out.println("W countdown: " + i);
                 Thread.sleep(100);
+                sThread.notifyThread(); // Notify SThread to continue
             }
             interrupt();
             sThread.interrupt();
